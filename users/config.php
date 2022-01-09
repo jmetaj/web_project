492139c3970f098ebb1d66c91e73160038ff9c9b
jmetaj <92587070+jmetaj@users.noreply.github.com>
2022-01-09 14:19:02 +0200

    Update config.php
    update

--- users/config.php
+++ users/config.php
@@ -1,1 +1,22 @@
+<?php
+ 
+ $db_host = "localhost";
+ $db_name= "mydb";
+ $db_user = "root";
+ $db_pass = "";
 
+
+ //connection to database
+ try{
+     $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass );
+     $conn-> SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "connection successfully";
+
+ } catch(PDOException $e){
+     echo "connection failed" . $e;
+     $e->getMessage();
+ }
+
+ 
+
+?>
