4ff43cafc09d2ede8f83856a4674760ee623325b
jmetaj <92587070+jmetaj@users.noreply.github.com>
2023-07-23 00:33:47 +0300

    Add files via upload

---
+++ users/connection.php
@@ -0,0 +1,22 @@
+<?php
+ 
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "project_db";
+
+// Create connection
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+else {
+    echo "Connected successfully!";
+}
+ 
+// Close the connection 
+mysqli_close($conn);
+
+?>
