f0f731fe9b789c50d2d94690e50b4df9a05b680b
jmetaj <92587070+jmetaj@users.noreply.github.com>
2023-07-23 17:39:56 +0300

    Update proccess.php

--- users/proccess.php
+++ users/proccess.php
@@ -1,24 +1,30 @@
 <?php
-require_once('config.php');
+require_once('connection.php');
 ?>
 
 <?php
+if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-if(isset($_POST)){
+    // Hash the password using bcrypt
+    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
 
-     $username = $_POST['username'];
-     $email = $_POST['email'];
-     $pass = $_POST['pass'];
+    $sql = "INSERT INTO users(username, email, password) VALUES(?, ?, ?)";
 
-     $sql = " INSERT INTO users (username, email, pass) values('$username','$email','$pass')";
-     
-     $stmtinsert = $conn->prepare($sql);
-     $result = $stmtinsert->execute([$username, $email, $pass]);
-
-     if($result){
-         echo "successfully saved";
-     } else {
-         echo "error!";
-     }
+    $stmtinsert = $conn->prepare($sql);
+    // Bind the values to the placeholders and execute
+    $stmtinsert->bind_param("sss", $username, $email, $hashedPassword);
+    $result = $stmtinsert->execute();
+    if ($result) {
+        echo "Successfully saved";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
+
+    // Close the statement
+   // $stmtinsert->close();
+   mysqli_close($conn);
+}
 ?>
